7191cf1265be3265486f6813366b9b3623de785e
Android225 <a.krishtofor@gmail.com>
2025-12-22 17:30:19 +0300

    Add Statistic Compose UI + Fix components for share on screen

--- app/src/main/java/com/example/hubtrackerapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/hubtrackerapp/presentation/screens/home/HomeScreen.kt
@@ -2,6 +2,7 @@
 
 package com.example.hubtrackerapp.presentation.screens.home
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -23,6 +25,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -38,7 +41,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -47,9 +55,11 @@ import com.example.hubtrackerapp.domain.hubbit.models.ModeForSwitch
 import com.example.hubtrackerapp.presentation.theme.Black10
 import com.example.hubtrackerapp.presentation.theme.Black100
 import com.example.hubtrackerapp.presentation.theme.Black20
+import com.example.hubtrackerapp.presentation.theme.Black40
 import com.example.hubtrackerapp.presentation.theme.Black60
 import com.example.hubtrackerapp.presentation.theme.Blue10
 import com.example.hubtrackerapp.presentation.theme.Blue100
+import com.example.hubtrackerapp.presentation.theme.Blue40
 import com.example.hubtrackerapp.presentation.theme.White100
 import java.time.LocalDate
 
@@ -96,6 +106,12 @@ fun HomeScreen(
                         onDateClick = {}
                     )
                 }
+                item {
+                    StatisticBox(
+                        onClick = {},
+                        progress = 0.25f
+                    )
+                }
             }
         }
     }
@@ -104,7 +120,8 @@ fun HomeScreen(
 @Composable
 fun TopActionRow() {
     Row(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth()
+            .padding(horizontal = 24.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -143,10 +160,13 @@ fun ProfileRow() {
     ) {
         Column() {
             Text(
-                text = "Hi, Mert"
+                text = "Hi, Mert",
+                style = MaterialTheme.typography.titleLarge
             )
             Text(
-                text = "Let's make habits together!"
+                text = "Let's make habits together!",
+                style = MaterialTheme.typography.bodyLarge,
+                color = Black40
             )
         }
         Box(
@@ -180,11 +200,7 @@ private fun ModSwitcher(
             .padding(top = 16.dp)
             .clip(RoundedCornerShape(16.dp))
             .background(Black10)
-            .fillMaxWidth()
-            .border(
-                width = 1.dp,
-                color = Black20,
-                shape = RoundedCornerShape(16.dp)),
+            .fillMaxWidth(),
         horizontalArrangement = Arrangement.Center
     ) {
         ModeItem(
@@ -213,10 +229,11 @@ private fun ModeItem(
 ) {
     Box(
         modifier = modifier
-            .background(if (selected) White100 else Color.Transparent)
+            .padding(3.dp)
+            .background(if (selected) White100 else Color.Transparent,RoundedCornerShape(32.dp))
             .clickable(onClick = onClick)
-            .padding(8.dp)
-            .clip(RoundedCornerShape(32.dp)),
+            .padding(5.dp),
+
         contentAlignment = Alignment.Center
     ) {
         Text(
@@ -290,5 +307,94 @@ fun DateCard(
             )
         }
 
+    }
+}
+
+@Composable
+fun StatisticBox(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    progress: Float
+    //передавать количество выполненных habbits
+){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp)
+            .padding(top = 8.dp)
+            .background(Blue100,
+                shape = RoundedCornerShape(16.dp))
+
+
+
+
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ProgressCircle(progress = progress)
+            Column(
+                modifier = Modifier
+                    .padding(start = 8.dp)
+            ) {
+                Text(
+                    text = "Your daily goals almost done!",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = White100
+                )
+                Text(
+                    text = "1 of 4 completed",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Blue40
+                )
+            }
+        }
+
+    }
+
+}
+
+@Composable
+fun ProgressCircle(
+    modifier: Modifier = Modifier,
+    progress: Float,
+    strokeWidth: Dp = 3.dp
+){
+    Box(
+        modifier = modifier.size(50.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        Canvas(
+            modifier = Modifier.fillMaxSize()
+                .padding(2.dp)
+        ) {
+            val stroke = Stroke(
+                width = strokeWidth.toPx(),
+                cap = StrokeCap.Round
+            )
+
+            drawArc(
+                color = Blue40,
+                startAngle = -90f,
+                sweepAngle = 360f,
+                useCenter = false,
+                style = stroke
+            )
+
+            drawArc(
+                color = White100,
+                startAngle = -90f,
+                sweepAngle = 360f * progress,
+                useCenter = false,
+                style = stroke
+            )
+        }
+        Text(
+            text = "%${(progress * 100).toInt()}",
+            style = MaterialTheme.typography.bodyLarge,
+            color = White100
+        )
     }
 }
